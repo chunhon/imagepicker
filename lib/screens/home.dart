fdda50a0afccc8645abcc3a9922143158633f82a
chunhon <chunhon@outlook.com>
2021-04-29 17:52:02 +0800

    Add files via upload

---
+++ lib/screens/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:path/path.dart' as Path;
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+}
